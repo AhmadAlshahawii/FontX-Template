e87d901956b726b9195d1ea4fac83ee2ded357ce
AhmadAlshahawii <eng.ahmad.alshahawi92@gmail.com>
2019-06-29 19:04:19 +0300

    add burger button

--- js/index.js
+++ js/index.js
@@ -2,6 +2,25 @@
 'use strict';
 
 
+
+$('#btn').click(function(){
+    
+    if ($('.nav-menu').is(':hidden')){
+      
+      $('#navBtn').removeClass('fa-bars');
+      $('#navBtn').addClass('fa-times');
+      
+    } else {
+      
+      $('#navBtn').removeClass('fa-times');
+      $('#navBtn').addClass('fa-bars');
+      
+    }
+    
+  });
+
+
+
 $("#btn").click(function(){
     
     $(".nav-menu").toggle(300);
